486b2c31015d84ed7106475ca880b16a10700ece
Logeswaran <logeswaran.gnt@gmail.com>
2018-09-26 08:41:53 +0530

    UI Modified

--- app_todo/models.py
+++ app_todo/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=200, default="sub1")
+    incharge = models.CharField(max_length=200)
+
 class Question(models.Model):
+    subject = models.ForeignKey(Subject, default="sub1")
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     difficulty = models.IntegerField(default=1)
 
 
-class Choice(models.Model):
+class Choice1(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
